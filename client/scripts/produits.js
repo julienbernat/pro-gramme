9d40ffbe4fcb7a2b61d25e071f49a5f376deca07
Hassaoui <clement@hassaoui.com>
2021-11-26 15:36:16 -0500

    ajout produits et séparation
    séparations des produits selon leurs catégories

--- client/scripts/produits.js
+++ client/scripts/produits.js
@@ -1,38 +1,24 @@
 function genererProduit(data) {
-  let produits = document.getElementById("PreWorkOutProd");
+  <!-- la fonction divise les produits dans leurs catégories respective -->
+  let preWorkout = document.getElementById("PreWorkOutProd");
+  let prots = document.getElementById("ProtsProd");
+  let postWorkout = document.getElementById("PostWorkOutProd");
+  let autre = document.getElementById("AutresProduits");
 
-  let innerHTML = "";
+  <!-- catérogie 1: preWorkout -->
+  <!-- catérogie 2: prots -->
+  <!-- catérogie 3: postWorkout -->
+  <!-- catérogie 4: autre -->
+
+  let innerHTMLpreWorkout = "";
+  let innerHTMLprots = "";
+  let innerHTMLpostWorkout = "";
+  let innerHTMLautre = "";
   for (let indice in data) {
     let element = data[indice];
-    innerHTML +=
-      '<article class="prod-item">' +
-      "<h1>" +
-      element.nom +
-      "</h1>" +
-      "<address>" +
-      element.description +
-      "</address>" +
-      "<h2>Prix : " +
-      element.prix +
-      "$" +
-      "</h2>" +
-      "<p>Quantité restant : " +
-      element.qte_inventaire +
-      "</p>" +
-      '<button onclick="ajouteraupanier(' +
-      element.id +
-      ')">Ajouter au panier</button>';
-    innerHTML += "</article>";
-  }
-  produits.innerHTML = innerHTML;
-}
-function genererProduit2(data) {
-  let produits = document.getElementById("ProtsProd");
 
-  let innerHTML = "";
-  for (let indice in data) {
-    let element = data[indice];
-    innerHTML +=
+    if(element.categorie.id == 1){
+      innerHTMLpreWorkout +=
         '<article class="prod-item">' +
         "<h1>" +
         element.nom +
@@ -50,11 +36,78 @@ function genererProduit2(data) {
         '<button onclick="ajouteraupanier(' +
         element.id +
         ')">Ajouter au panier</button>';
-    innerHTML += "</article>";
-  }
-  produits.innerHTML = innerHTML;
-}
+      innerHTMLpreWorkout +="</article>";
+    }
+    else if(element.categorie.id == 2){
+      innerHTMLprots +=
+          '<article class="prod-item">' +
+          "<h1>" +
+          element.nom +
+          "</h1>" +
+          "<address>" +
+          element.description +
+          "</address>" +
+          "<h2>Prix : " +
+          element.prix +
+          "$" +
+          "</h2>" +
+          "<p>Quantité restant : " +
+          element.qte_inventaire +
+          "</p>" +
+          '<button onclick="ajouteraupanier(' +
+          element.id +
+          ')">Ajouter au panier</button>';
+      innerHTMLprots +="</article>";
+    }
+    else if(element.categorie.id == 3){
+      innerHTMLpostWorkout +=
+          '<article class="prod-item">' +
+          "<h1>" +
+          element.nom +
+          "</h1>" +
+          "<address>" +
+          element.description +
+          "</address>" +
+          "<h2>Prix : " +
+          element.prix +
+          "$" +
+          "</h2>" +
+          "<p>Quantité restant : " +
+          element.qte_inventaire +
+          "</p>" +
+          '<button onclick="ajouteraupanier(' +
+          element.id +
+          ')">Ajouter au panier</button>';
+      innerHTMLpostWorkout +="</article>";
+    }
+    else if(element.categorie.id == 4){
+      innerHTMLautre +=
+          '<article class="prod-item">' +
+          "<h1>" +
+          element.nom +
+          "</h1>" +
+          "<address>" +
+          element.description +
+          "</address>" +
+          "<h2>Prix : " +
+          element.prix +
+          "$" +
+          "</h2>" +
+          "<p>Quantité restant : " +
+          element.qte_inventaire +
+          "</p>" +
+          '<button onclick="ajouteraupanier(' +
+          element.id +
+          ')">Ajouter au panier</button>';
+      innerHTMLautre +="</article>";
+    }
 
+  }
+  preWorkout.innerHTML = innerHTMLpreWorkout;
+  prots.innerHTML = innerHTMLprots;
+  postWorkout.innerHTML = innerHTMLpostWorkout;
+  autre.innerHTML = innerHTMLautre;
+}
 
 function chargerproduits() {
   fetch("./produits")
